574222961110cb7e11b3cea2692ee4cc9bf8bbd2
TomBorthwick24 <30809211+tombk24@users.noreply.github.com>
2022-06-09 23:50:01 +0100

    Contains Duplicates

---
+++ src/main/java/ContainsDuplicates.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class ContainsDuplicates {
+
+//    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+//
+//    Example 1:
+//
+//    Input: nums = [1,2,3,1]
+//    Output: true
+
+    public boolean containsDuplicateIteration1(int[] nums) {
+
+        // Scored faster than 13%.
+        Arrays.sort(nums);
+        if(nums.length == 1){
+            return false;
+        }
+        var containsDuplicates = false;
+        for(var index = 0; index < nums.length - 1; index++){
+            if(nums[index] == nums[index + 1]) {
+                containsDuplicates = true;
+            }
+        }
+
+        return containsDuplicates;
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        // Scored faster than 76%.
+        var numHashmap = new HashSet<>();
+        for(var number: nums){
+            if(numHashmap.contains(number)){
+                return true;
+            }
+            numHashmap.add(number);
+        }
+        return false;
+    }
+
+}
